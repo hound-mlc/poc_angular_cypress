c7f86ceb2223e1c0787d515bf9d867cdbf7e6046
Manuel Lopez <w.mlc32@gmail.com>
2021-04-07 23:45:40 +0200

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { RequestService } from './services/request.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pocCypress';
+  loading = false;
+
+  constructor(public rqService: RequestService) {
+    this.rqService.getLoading().subscribe(value => {
+      this.loading = value;
+    })
+  }
+
 }
